aa6f81d8b9576ad3d4e99584b05ab5df0170c97b
feisen9 <feisen9@163.com>
2022-12-22 13:25:40 +0800

    test 22-12-22-13:14

---
+++ src/main/java/com/hhu20/jsgl/JsglApplication.java
@@ -0,0 +1,13 @@
+package com.hhu20.jsgl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsglApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsglApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hhu20/jsgl/JsglApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhu20.jsgl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsglApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
