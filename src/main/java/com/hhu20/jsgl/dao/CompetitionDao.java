651120b6f314bd52335832b2bc49fc2d4f70ffe4
林苗 <8213476+Lin_Miao@user.noreply.gitee.com>
2022-12-28 02:48:00 +0800

    CompetitionDao

---
+++ src/main/java/com/hhu20/jsgl/dao/CompetitionDao.java
@@ -0,0 +1,45 @@
+package com.hhu20.jsgl.dao;
+import com.hhu20.jsgl.mapper.CompetitionMapper;
+import org.apache.ibatis.session.SqlSession;
+
+import java.util.List;
+import java.util.Map;
+
+public class CompetitionDao {
+    private SqlSession sqlSession;
+    private CompetitionMapper competitionMapper;
+    public CompetitionDao(SqlSession sqlSession){
+        this.sqlSession = sqlSession;
+        competitionMapper = (CompetitionMapper) sqlSession.getMapper(CompetitionMapper.class);
+    }
+
+    public List<Map> selectAll(){
+        List<Map> outMap = competitionMapper.selectAll();
+        return outMap;
+    }
+    public  List<Map> add(String cno, String bno, String cname, String clevel,
+                         String organizer, String hostintitue){
+        List<Map> outMap = competitionMapper.add(cno, bno, cname, clevel,
+                organizer, hostintitue);
+        return outMap;
+    }
+    public List<Map> delete(String cno){
+        List<Map> outMap = competitionMapper.delete(cno);
+        return outMap;
+    }
+
+    public List<Map> update(String cno, String bno, String cname, String clevel,
+                            String organizer, String hostintitue){
+        List<Map> outMap = competitionMapper.update(cno, bno, cname, clevel,
+                organizer, hostintitue);
+        return outMap;
+    }
+
+
+    public List<Map> selectByCondition(String cno, String bno, String cname, String clevel,
+                                       String organizer, String hostintitue){
+        List<Map> outMap = competitionMapper.selectByCondition(cno, bno, cname, clevel,
+                organizer, hostintitue);
+        return outMap;
+    }
+}
---
+++ src/main/java/com/hhu20/jsgl/mapper/CompetitionMapper.java
@@ -0,0 +1,22 @@
+package com.hhu20.jsgl.mapper;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CompetitionMapper {
+    List<Map> selectAll();
+
+    List<Map> add(String cno, String bno, String cname, String clevel,
+            String organizer, String hostinstitue);
+
+//    int add(String cno, String bno, String cname, String clevel,
+//                   String organizer, String hostinstitue);
+    List<Map> delete(String cno);
+
+    List<Map> update(String cno, String bno, String cname, String clevel,
+                     String organizer, String hostinstitue);
+
+    List<Map> selectByCondition(String cno, String bno, String cname, String clevel,
+                     String organizer, String hostinstitue);
+
+}
